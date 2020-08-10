7ec5a94029d1679aff9ee400933b3552f81e4c9d
David Potapkin <david.potapkin@dataart.com>
2020-08-10 13:47:53 +0300

    Request for help!

--- views/homePage.js
+++ views/homePage.js
@@ -1,10 +1,14 @@
 const saveForm = require('./saveForm');
 const getAllForm = require('./getAllForm');
 
-const homePage = (res) => {    
+const homePage = (res) => {
     res.write('<html>');
     res.write('<head><title>Enter Page</title></head>');
     res.write('<body>');
+
+    res.write('<h3>Could you help in resolving some issues from this test task?</h3>');
+    res.write('<div>mail: david.potapkin@dataart.com</div>');
+    res.write('<div>skype: dv1d211</div>')
 
     res.write('<h1>Add new route:</h1>');
     res.write(saveForm('route', 'POST'));
